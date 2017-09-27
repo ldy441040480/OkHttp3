436605991efcc0b1d88ae4ea7ee3bf88109cd694
lidongyang <lidongyang@babytree-inc.com>
2017-09-27 20:01:32 +0800

    update

--- app/src/main/java/ldy/werty/okhttp/sample/MainActivity.java
+++ app/src/main/java/ldy/werty/okhttp/sample/MainActivity.java
@@ -49,15 +49,20 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
 
-        fileDown();
+//        fileDown();
 //        getBean();
 //        getString();
 //        getBitmap();
 //        getJSONObject();
 //        post();
 //        postFile();
+
+        fileDown();
     }
 
+    /**
+     * 获取bean
+     */
     public void getBean() {
         OkRequestParams params = new OkRequestParams();
         params.put("", "");
@@ -65,31 +70,33 @@ public class MainActivity extends AppCompatActivity {
         OkHttpUtil.get("http://abc.ersan23.com/player/loginpic", params, new OkHttpResBeanHandler<TestBean>() {
             @Override
             public void onSuccess(Call call, int code, Headers headers, TestBean response) {
-
             }
 
             @Override
             public void onFailure(Call call, int code, Headers headers, int error, Throwable t) {
-
             }
         });
     }
 
+    /**
+     * 获取String
+     */
     public void getString() {
         OkHttpUtil.get("http://r.babytree.com/bxuh4mV", new OkHttpResStringHandler() {
 
             @Override
             public void onSuccess(Call call, int code, Headers headers, String response) {
-
             }
 
             @Override
             public void onFailure(Call call, int code, Headers headers, int error, Throwable t) {
-
             }
         });
     }
 
+    /**
+     * 获取JSONObject
+     */
     public void getJSONObject() {
         OkRequestParams params = new OkRequestParams();
         params.put("", "");
@@ -98,31 +105,33 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess(Call call, int code, Headers headers, JSONObject response) {
-
             }
 
             @Override
             public void onFailure(Call call, int code, Headers headers, int error, Throwable t) {
-
             }
         });
     }
 
+    /**
+     * 获取bitmap
+     */
     public void getBitmap() {
         OkHttpUtil.get("http://images.csdn.net/20150817/1.jpg", new OkHttpResBitmapHandler() {
 
             @Override
             public void onSuccess(Call call, int code, Headers headers, Bitmap response) {
-
             }
 
             @Override
             public void onFailure(Call call, int code, Headers headers, int error, Throwable t) {
-
             }
         });
     }
 
+    /**
+     * post请求
+     */
     public void post() {
         OkRequestParams params = new OkRequestParams();
         params.putHeader("aa", "aa323");
@@ -135,16 +144,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess(Call call, int code, Headers headers, JSONObject response) {
-
             }
 
             @Override
             public void onFailure(Call call, int code, Headers headers, int error, Throwable t) {
-
             }
         });
     }
 
+    /**
+     * 上传文件
+     */
     public void postFile() {
         OkRequestParams params = new OkRequestParams();
         params.put("11", "113333");
@@ -155,12 +165,10 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess(Call call, int code, Headers headers, JSONObject response) {
-
             }
 
             @Override
             public void onFailure(Call call, int code, Headers headers, int error, Throwable t) {
-
             }
 
             @Override
@@ -170,27 +178,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * 下载文件
+     */
     public void fileDown() {
         String url = "http://down1.cnmo.com/app/diebaoguan/Diebao_Branches.apk";
         String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
-        String fileName = "Diebao_Branches.apk";
+        String fileName = "hepburn-sunglasses-off.gif";
 
         OkHttpUtil.get(url, "fileDown", new OkHttpResFileHandler(dirPath, fileName) {
 
             @Override
             public void onSuccess(Call call, int code, Headers headers, File response) {
-
+                Log.i("Werty", "onSuccess call=[" + call + "];code=[" + code + "];headers=[" + headers + "];response=[" + response + "];");
             }
 
             @Override
             public void onFailure(Call call, int code, Headers headers, int error, Throwable t) {
-
+                Log.i("Werty", "onSuccess call=[" + call + "];code=[" + code + "];headers=[" + headers + "];error=[" + error + "];t=[" + t + "];");
             }
 
             @Override
             public void onProgress(long curSize, long totalSize, float progress, boolean done) {
                 String str = "curSize=" + curSize + ";\ntotalSize=" + totalSize + ";\nprogress=" + progress + ";\ndone=" + done;
-                Log.i("TAG", "onProgress t[" + str + "]");
+                Log.i("Werty", "onProgress t[" + str + "]");
             }
         });
     }
